c92ee46aed9287e0bc054c9f80ecbb95da1af611
Abhisha1 <35671786+Abhisha1@users.noreply.github.com>
2018-02-14 14:53:13 +1100

    Update tie_break_short.py

--- tie_break_short.py
+++ tie_break_short.py
@@ -11,12 +11,8 @@ def comp101_tiebreaker(points, server):
     for a in points:
         game_score[a] += 1
         i = i + 1
-        if (game_score[server] >= WINNING_TIEBREAK_SCORE or 
-            game_score[other_plyr] >= WINNING_TIEBREAK_SCORE) and abs(game_score[server] - game_score[other_plyr]) >= WIN_BY:
-            if (game_score[server] > game_score[other_plyr]):
-                winner = 0
-            else:
-                winner = 1
+        if (game_score[a] >= WINNING_TIEBREAK_SCORE) and abs(game_score[server] - game_score[other_plyr]) >= WIN_BY:
+            winner = a
             rem = points[i:]
             break
     return '{}-{}'.format(game_score[server], game_score[other_plyr]), winner, rem
