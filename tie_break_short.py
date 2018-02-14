f943174204f6be75e643d1938980247e1bceec4c
Abhisha1 <35671786+Abhisha1@users.noreply.github.com>
2018-02-14 14:50:13 +1100

    Update tie_break_short.py

--- tie_break_short.py
+++ tie_break_short.py
@@ -3,20 +3,20 @@ WIN_BY = 2
 
 
 def comp101_tiebreaker(points, server):
+    # initialize the game variables
     other_plyr = (server+1) % 2
     game_score = [0, 0]
     winner = None
     i = 0
     rem = []
     for a in points:
+        #increment the game score accordingly to list of points
         game_score[a] += 1
         i = i + 1
-        if (game_score[server] >= WINNING_TIEBREAK_SCORE or 
-            game_score[other_plyr] >= WINNING_TIEBREAK_SCORE) and abs(game_score[server] - game_score[other_plyr]) >= WIN_BY:
-            if (game_score[server] > game_score[other_plyr]):
-                winner = 0
-            else:
-                winner = 1
+        # checks if a player has one the tiebreak
+        if (game_score[a] >= WINNING_TIEBREAK_SCORE) and abs(game_score[server] - game_score[other_plyr]) >= WIN_BY:
+            winner = a
+            #remaining points are indicated as remainder
             rem = points[i:]
             break
     return '{}-{}'.format(game_score[server], game_score[other_plyr]), winner, rem
